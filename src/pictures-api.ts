2774d543e75d56ed57e677ea066f9076a23e78e7
hanna-uler <hannauler@gmail.com>
2025-06-20 19:26:04 -0400

    add generic to axios.get

--- src/pictures-api.ts
+++ src/pictures-api.ts
@@ -9,7 +9,7 @@ type ResponseData = {
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
 export default async function fetchPictures(searchQuery: string, page: number): Promise<ResponseData> {
-    const response = await axios.get(`/photos`, {
+    const response = await axios.get<ResponseData>(`/photos`, {
         params: {
             client_id: "s2Hc5k6SJc9T2KwEbCWWrchT2ziggT9x-1USmS7To8Y",
             query: searchQuery,
