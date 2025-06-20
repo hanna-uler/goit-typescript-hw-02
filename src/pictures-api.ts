7a292e5d46598b739e8dc32c93d9de5eb4c8c273
hanna-uler <hannauler@gmail.com>
2025-06-20 12:06:22 -0400

    type fetch

--- src/pictures-api.ts
+++ src/pictures-api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
-export default async function fetchPictures(searchQuery, page) {
+export default async function fetchPictures(searchQuery: string, page: number) {
     const response = await axios.get(`/photos`, {
         params: {
             client_id: "s2Hc5k6SJc9T2KwEbCWWrchT2ziggT9x-1USmS7To8Y",
