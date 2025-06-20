29eb830adafc8844729201ca8e7c3b2b6ca6e340
hanna-uler <hannauler@gmail.com>
2025-06-20 19:23:08 -0400

    type http rqst func

--- src/pictures-api.ts
+++ src/pictures-api.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+type ResponseData = {
+    results: Img[];
+    total: number;
+    total_pages: number;
+}
+
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
-export default async function fetchPictures(searchQuery: string, page: number) {
+export default async function fetchPictures(searchQuery: string, page: number): Promise<ResponseData> {
     const response = await axios.get(`/photos`, {
         params: {
             client_id: "s2Hc5k6SJc9T2KwEbCWWrchT2ziggT9x-1USmS7To8Y",
